48ea7bc901a6326f8bcdaf2d4d68f1dd46a628c1
Puseletso12 <puseletsochuene88@gmail.com>
2025-07-27 00:05:38 +0200

    home page

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">Puseletso Chuene</h1>
+      <h3 className="home__subtitle">Full Stack Developer</h3>
+      <p className="home__description">
+        A passionate IT student and full stack developer that crafts
+        user-friendly websites and apps that combine creativity, strategy, and
+        clean code.
+      </p>
+
+      <a href="#contact" className="button button--flex">
+        Say Hello
+      </a>
+    </div>
+  );
+};
+
+export default Data;
---
+++ src/components/home/social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/todays.episode_/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/puseletso-chuene-0840b8278/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin"></i>
+      </a>
+      <a
+        href="https://github.com/Puseletso12"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
