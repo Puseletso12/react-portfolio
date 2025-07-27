7a4ffb44a9f464fda118bc81f7b0e160fffdc494
Puseletso12 <puseletsochuene88@gmail.com>
2025-07-27 10:22:37 +0200

    about section

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -6,9 +6,9 @@ const Data = () => {
       <h1 className="home__title">Puseletso Chuene</h1>
       <h3 className="home__subtitle">Full Stack Developer</h3>
       <p className="home__description">
-        A passionate IT student and full stack developer that crafts
-        user-friendly websites and apps that combine creativity, strategy, and
-        clean code.
+        A passionate Bachelor of Information Technology in Business Systems
+        student and full stack developer that crafts user-friendly websites and
+        apps that combine creativity, strategy, and clean code.
       </p>
 
       <a href="#contact" className="button button--flex">
